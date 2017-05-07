b529ea3b40ed79673e3f0a9aab0731d3fcdb1ccc
René Föhring <rene.foehring@5minds.de>
2017-05-07 20:29:21 +0200

    Init

---
+++ lib/credo_elixir_benchmark.ex
@@ -0,0 +1,18 @@
+defmodule CredoElixirBenchmark do
+  @moduledoc """
+  Documentation for CredoElixirBenchmark.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CredoElixirBenchmark.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/credo_elixir_benchmark_test.exs
@@ -0,0 +1,8 @@
+defmodule CredoElixirBenchmarkTest do
+  use ExUnit.Case
+  doctest CredoElixirBenchmark
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
